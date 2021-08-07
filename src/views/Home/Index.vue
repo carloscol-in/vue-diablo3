0d99f04d9945d584efa401f7eb0d16ee7a49932e
Carlos <contact@carloscolin.me>
2021-08-07 01:19:40 -0500

    :children_crossing: Added dark mode support

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" class="mx-auto" src="@/assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="text-5xl text-black dark:text-white">Home Page</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
