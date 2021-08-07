69d8020f28db4e82208ec9dc735a6f30c639f90e
Carlos <contact@carloscolin.me>
2021-08-06 20:17:02 -0500

    Imports; tailwind, bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
+// Vue NPM dependencies
 import Vue from 'vue'
+
+// Third party NPM dependencies
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@/assets/css/tailwind.css'
+
+// Local dependencies
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// Extra configuration
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
