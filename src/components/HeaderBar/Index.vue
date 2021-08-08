1adb80275003ba13ce65b5d9e5a3eee7ebb2be3f
Carlos <contact@carloscolin.me>
2021-08-08 18:56:46 -0500

    Made a HeaderBar

---
+++ src/components/HeaderBar/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="header-bar">
+        <div class="navigation-bar">
+            <b-navbar toggleable="lg" type="dark" variant="dark">
+                <b-navbar-brand :to="{ name: 'Home' }">
+                    <img src="@/assets/img/diablo-iii.svg" alt="Diablo 3 Logo">
+                    <span class="font-diablo ml-2">Diablo 3 PF</span>
+                </b-navbar-brand>
+            </b-navbar>
+        </div>
+    </div>
+</template>
+
+<script>
+export default { nane: 'HeaderBar' }
+</script>
